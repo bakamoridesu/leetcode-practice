78352035b2cab1d6127c12cda8e6d4259dadfde9
Aleksandr Lobov <a.lobov@basis.center>
2022-07-14 16:46:13 +0300

    added roman to int challenge + tests

---
+++ src/challenges/roman-to-int/index.ts
@@ -0,0 +1,11 @@
+import { symbolMap } from "./config";
+
+export function romanToInt(s: string): number {
+    if(!s) return 0
+    if(s.length === 1) return symbolMap[s]
+
+    if (symbolMap[s[0]] < symbolMap[s[1]]) {
+        return symbolMap[s[1]] - symbolMap[s[0]] + romanToInt(s.substring(2))
+    }
+    return symbolMap[s[0]] + romanToInt(s.substring(1));
+}
---
+++ src/challenges/roman-to-int/tests.ts
@@ -0,0 +1,6 @@
+export const testCases = [
+    {X: 10},
+    {III: 3},
+    {LVIII: 58},
+    {MCMXCIV: 1994},
+]
