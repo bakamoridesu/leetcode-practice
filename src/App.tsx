ab49e4e753b4fcd5c19bf55c8c0d0a4550186924
Aleksandr Lobov <bakamoridesu@gmail.com>
2022-07-19 13:13:38 +0300

    day 6 | number of steps to reduce a number to zero

--- src/App.tsx
+++ src/App.tsx
@@ -3,35 +3,39 @@ import './App.css';
 import cn from 'classnames'
 
 import currentChallenge from './current'
-import {testCases} from './current/tests'
-import {name} from "./current/config";
+import { testCases } from './current/tests'
+import { name } from "./current/config";
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Current challenge: {name}</h1>
-            <Tests currentChallenge={currentChallenge} testCases={testCases}/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <h1>Current challenge: {name}</h1>
+      <Tests currentChallenge={currentChallenge} testCases={testCases}/>
+    </div>
+  );
 }
 
 // @ts-ignore
-const Tests = ({testCases, currentChallenge}) => {
-    return (
-        <>
-            {testCases.map((test: any, index: any) => {
-                // change these 2 lines based on test case structure
-                const testResult = currentChallenge(...test.test)
-                const isCorrect = testResult.join('') === test.answer.join('')
+const Tests = ({ testCases, currentChallenge }) => {
+  return (
+    <>
+      {testCases.map((test: any, index: any) => {
+        // change these 2 lines based on test case structure
+        const testResult = currentChallenge(test.test)
+        // multi variable input:
+        //const testResult = currentChallenge(...test.test)
+        const isCorrect = testResult === test.answer
+        // array output:
+        // const isCorrect = testResult.join('') === test.answer.join('')
 
-                return (
-                    <div key={index} className={cn('test', isCorrect ? 'passed' : 'failed')}>
-                        {`Test: ${test.test.toString()}, result: ${testResult}, correctAnswer: ${test.answer}`}
-                    </div>
-                )
-            })}
-        </>
-    )
+        return (
+          <div key={index} className={cn('test', isCorrect ? 'passed' : 'failed')}>
+            {`Test: ${test.test.toString()}, result: ${testResult}, correctAnswer: ${test.answer}`}
+          </div>
+        )
+      })}
+    </>
+  )
 }
 
 export default App;
